7f679248cb0648999fbaa6ef37db864f220ba394
MhaamadAli <m.alialaawar@gmail.com>
2024-03-10 14:21:28 +0200

    added popup section for creating new transaction

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+document.getElementById('showPopup').addEventListener('click', function() {
+    document.getElementById('pop-up-transaction').classList.add('show');
+  });
+
+  document.getElementById('close-transaction').addEventListener('click', function() {
+    document.getElementById('pop-up-transaction').classList.remove('show');
+  });
